c11f62e003d39708bad5d73d36d0742139e1beeb
Ahmed Zia <ziaahmed486@gmail.com>
2023-03-03 16:56:08 -0800

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -10,9 +10,10 @@ function Alert(props) {
   
   return (
     props.alert &&
+    <div style={{height:'50px'}}>
     <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
   <strong>{capitalize(props.alert.type)}</strong>{props.alert.msg}
- 
+  </div>
 </div>
   )
 }
